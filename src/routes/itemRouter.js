bf8b414c085e89514795d1afefa672e8a84578ab
Muhammad Tabish <tabish615@gmail.com>
2018-06-11 15:53:42 +0500

    asd

--- src/routes/itemRouter.js
+++ src/routes/itemRouter.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var itemRouter = express.Router();
-
+var axios = require("axios")
 itemRouter.route('/').get(function (req, res) {
     console.log(req.headers.asd);
     // res.send(JSON.stringify(req))
     res.send("asd................")
+
+    axios.get(`https://graph.facebook.com/oauth/access_token?client_id=232064780897684&client_secret=3f93548590d82c2e8c93f02929436801&grant_type=client_credentials`,
+    ).then(function (a) {
+        console.log(a)
+    })
+        .catch(err => console.log(err))
 });
+
 
 module.exports = itemRouter;
