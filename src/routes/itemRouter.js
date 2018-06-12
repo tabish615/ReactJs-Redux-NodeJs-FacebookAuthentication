59258e776888a12f10def6e23e1d72aad8053a4b
Muhammad Tabish <tabish615@gmail.com>
2018-06-12 14:01:17 +0500

    change

--- src/routes/itemRouter.js
+++ src/routes/itemRouter.js
@@ -5,7 +5,7 @@ var axios = require("axios")
 itemRouter.route('/').get(function (req, res) {
     console.log(req.headers.asd);
     // res.send(JSON.stringify(req))
-    res.send("asd................")
+    // res.send("asd................")
 
     axios.get(`https://graph.facebook.com/oauth/access_token?client_id=232064780897684&client_secret=3f93548590d82c2e8c93f02929436801&grant_type=client_credentials`,
     ).then(function (a) {
