c2034ea4164ea60f46821c94ea9f50b5f25b34ee
Muhammad Tabish <tabish615@gmail.com>
2018-06-19 14:21:15 +0500

    change

--- src/routes/itemRouter.js
+++ src/routes/itemRouter.js
@@ -6,16 +6,21 @@ var axios = require("axios")
 itemRouter.route('/').get(function (req, res) {
     console.log(req.headers.asd);
 
+    // Verify Facebook access token
+
+    // first we get App access token
     axios.get(`https://graph.facebook.com/oauth/access_token?client_id=232064780897684&client_secret=3f93548590d82c2e8c93f02929436801&grant_type=client_credentials`,
-    ).then(function (a) {
-        console.log(a)
-        axios.get(`https://graph.facebook.com/debug_token?input_token=${req.headers.asd}&access_token=${a.data.access_token}`)
-            .then((e) => {
-                console.log(e)
-                axios.get(`https://graph.facebook.com/${e.data.data.user_id}?access_token=${req.headers.asd}`)
-                    .then((d) => {
-                        console.log(d)
-                        res.send(d.data)
+    ).then(function (appAccessToken) {
+        console.log(appAccessToken)
+
+        // Now we get user id
+        axios.get(`https://graph.facebook.com/debug_token?input_token=${req.headers.asd}&access_token=${appAccessToken.data.access_token}`)
+            .then((userId) => {
+                console.log(userId)
+                axios.get(`https://graph.facebook.com/${userId.data.data.user_id}?access_token=${req.headers.asd}`)
+                    .then((userProfile) => {
+                        console.log(userProfile)
+                        res.send(userProfile.data)
                     }).catch(err => console.log(err))
             }).catch((s) => {
                 console.log(s)
