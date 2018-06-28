f7fcd2bc73b424613dd20ef34243590bf0ee7801
Muhammad Tabish <tabish615@gmail.com>
2018-06-28 18:32:56 +0500

    'change'

--- src/container/SignIn/signin.js
+++ src/container/SignIn/signin.js
@@ -16,26 +16,6 @@ class SignIn extends Component {
         };
     }
 
-    // componentDidMount() {
-    //     (function (d, s, id) {
-    //         var js, fjs = d.getElementsByTagName(s)[0];
-    //         if (d.getElementById(id)) return;
-    //         js = d.createElement(s); js.id = id;
-    //         js.src = "//connect.facebook.net/en_US/sdk.js";
-    //         fjs.parentNode.insertBefore(js, fjs);
-    //     }(document, 'script', 'facebook-jssdk'));
-
-    //     window.fbAsyncInit = function () {
-    //         window.FB.init({
-    //             appId: 232064780897684,
-    //             status : true,
-    //             cookie: true,
-    //             xfbml: true,
-    //             version: 'v3.0'
-    //         });
-    //     };
-    // }
-
     submission(e) {
         if (this.state.email !== "" && this.state.password !== "") {
             e.preventDefault();
